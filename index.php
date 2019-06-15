46901fb9af6286647dd1cdb7fb806bd84bc62f22
simonequarticelli <quarti87@gmail.com>
2019-06-15 16:05:07 +0200

    init ex php-faq + header ok

---
+++ index.php
@@ -0,0 +1,36 @@
+<!-- Riscrivere questa pagina del sito google
+https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte.
+Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP. -->
+
+<?php  ?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>php-faq</title>
+    <link rel="stylesheet" href="public/app.css">
+  </head>
+  <body>
+    <header>
+      <nav id="logo">
+        <a href="#"><img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png" alt=""></a>
+        <span>Privacy e termini</span>
+      </nav>
+      <nav id="menu">
+        <ul>
+          <li><a href="#">Introduzione</a></li>
+          <li><a href="#">Norme sulla privacy</a></li>
+          <li><a href="#">Termini di servizio</a></li>
+          <li><a href="#">Tecnologie</a></li>
+          <li><a href="#">Domande frequenti</a></li>
+          <li><a href="#">Account Google</a></li>
+        </ul>
+      </nav>
+    </header>
+
+
+  </body>
+</html>
